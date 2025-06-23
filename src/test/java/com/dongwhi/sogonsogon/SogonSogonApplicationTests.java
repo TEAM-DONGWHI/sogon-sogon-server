ba3552249a33867a1aa06cd41a7519c1fb522a92
dlskl <skyandme77@gmail.com>
2025-06-23 15:12:56 +0900

    first commit

---
+++ src/test/java/com/dongwhi/sogonsogon/SogonSogonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongwhi.sogonsogon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SogonSogonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
